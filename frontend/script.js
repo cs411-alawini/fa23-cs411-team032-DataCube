3916db326e91ab05be2a8ea45e2dfbfec2944883
Jiayi Gu <89435466+Jiayi079@users.noreply.github.com>
2023-12-03 16:54:59 -0600

    adding search bar and pop-up window

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+    // Dynamically generate category checkboxes
     const categoriesContainer = document.querySelector('.categories');
-
     for (let i = 1; i <= 21; i++) {
         let categoryDiv = document.createElement('div');
         categoryDiv.className = 'category';
@@ -21,16 +21,33 @@ document.addEventListener('DOMContentLoaded', (event) => {
         categoriesContainer.appendChild(categoryDiv);
     }
 
-    // submit receieve form
+    // Form submission event
     const form = document.getElementById('category-form');
     form.addEventListener('submit', function(event) {
         event.preventDefault();
-        const selectedCategories = [];
-        const checkboxes = document.querySelectorAll('input[name="category"]:checked');
-        checkboxes.forEach((checkbox) => {
-            selectedCategories.push(checkbox.value);
-        });
+        const selectedCategories = Array.from(document.querySelectorAll('input[name="category"]:checked')).map(checkbox => checkbox.value);
+        console.log(selectedCategories); // Do something with the selected categories
+    });
 
-        console.log(selectedCategories);
+    // Modal functionality
+    const modal = document.getElementById("myModal"); // Change "myModal" to your modal's ID
+    const searchBtn = document.getElementById("searchBtn"); // Change "searchBtn" to your search button's ID
+    const closeBtn = document.querySelector(".close"); // Use the close class inside your modal
+
+    // When the user clicks the search button, open the modal
+    searchBtn.addEventListener('click', () => {
+        modal.style.display = "block";
+    });
+
+    // When the user clicks on the close button, close the modal
+    closeBtn.addEventListener('click', () => {
+        modal.style.display = "none";
+    });
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.addEventListener('click', (event) => {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
     });
 });
