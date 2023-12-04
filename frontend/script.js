97f9e4583f923531a15d08bc988fbd0b9147bdf2
Jiayi Gu <89435466+Jiayi079@users.noreply.github.com>
2023-12-03 22:47:17 -0600

    adding search top 10 videos by selecting specific category

--- frontend/script.js
+++ frontend/script.js
@@ -1,64 +1,105 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+    // Object to hold categoryName to categoryID mapping
+    const categoryIdMapping = {};
+
     // Fetch categories from the server and create checkboxes
     const categoriesContainer = document.querySelector('.categories');
-    
+    const form = document.getElementById('category-form');
+    const modal = document.getElementById("popup-modal");
+    const searchBtn = document.getElementById("search-button");
+    const closeBtn = document.querySelector(".close");
+
+    // Function to update video rankings on the page
+    function updateVideoRankings(videos) {
+        const rankingContainer = document.getElementById('ranking-placeholder');
+        rankingContainer.innerHTML = ''; // Clear existing content
+        videos.forEach((video, index) => {
+            const listItem = document.createElement('div');
+            listItem.className = 'video-ranking-item';
+            listItem.innerHTML = `
+                <div class="rank">${index + 1}</div>
+                <div class="title">${video.title}</div>
+                <div class="views">${video.view_count.toLocaleString()} views</div>
+            `;
+            rankingContainer.appendChild(listItem);
+        });
+    }
+
+    // Fetch and display top ten videos for all categories when the page loads
+    fetch('http://localhost:4000/video/top_all_categories') // Replace with your actual endpoint
+        .then(response => response.json())
+        .then(data => {
+            if (data && data.data) {
+                updateVideoRankings(data.data);
+            } else {
+                console.error('Failed to fetch top videos for all categories');
+            }
+        })
+        .catch(error => console.error('Error fetching top videos for all categories:', error));
+
+    // Fetch categories and create checkboxes
     fetch('http://localhost:4000/category')
         .then(response => response.json())
         .then(data => {
-            if(data.message === "Successfully get all categories") {
-                data.data.forEach(category => {
-                    let categoryDiv = document.createElement('div');
-                    categoryDiv.className = 'category';
-                    
-                    let input = document.createElement('input');
-                    input.type = 'checkbox';
-                    input.id = 'category' + category.categoryID;
-                    input.name = 'category';
-                    input.value = category.categoryName.trim(); // Assuming categoryName is a string
-                    
-                    let label = document.createElement('label');
-                    label.htmlFor = 'category' + category.categoryID;
-                    label.textContent = category.categoryName.trim(); // Using trim to remove any trailing newline characters
-                    
-                    categoryDiv.appendChild(input);
-                    categoryDiv.appendChild(label);
-                    
-                    categoriesContainer.appendChild(categoryDiv);
-                });
-            } else {
-                console.error('Failed to fetch categories');
-            }
+            data.data.forEach(category => {
+                categoryIdMapping[category.categoryName.trim()] = category.categoryID;
+
+                let categoryDiv = document.createElement('div');
+                categoryDiv.className = 'category';
+
+                let input = document.createElement('input');
+                input.type = 'checkbox';
+                input.id = 'category' + category.categoryID;
+                input.name = 'category';
+                input.value = category.categoryName.trim();
+
+                let label = document.createElement('label');
+                label.htmlFor = 'category' + category.categoryID;
+                label.textContent = category.categoryName.trim();
+
+                categoryDiv.appendChild(input);
+                categoryDiv.appendChild(label);
+                categoriesContainer.appendChild(categoryDiv);
+            });
         })
         .catch(error => console.error('Error fetching categories:', error));
 
-    // Form submission event
-    const form = document.getElementById('category-form');
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        const selectedCategories = Array.from(document.querySelectorAll('input[name="category"]:checked')).map(checkbox => checkbox.value);
-        console.log(selectedCategories); // Do something with the selected categories
-    });
-
-    // Modal functionality
-    const modal = document.getElementById("myModal"); // Change "myModal" to your modal's ID
-    const searchBtn = document.getElementById("searchBtn"); // Change "searchBtn" to your search button's ID
-    const closeBtn = document.querySelector(".close"); // Use the close class inside your modal
-
-    // When the user clicks the search button, open the modal
+    // Modal event listeners
     searchBtn.addEventListener('click', () => {
         modal.style.display = "block";
-        // You can add more code here to populate the modal with specific search results
     });
 
-    // When the user clicks on the close button, close the modal
     closeBtn.addEventListener('click', () => {
         modal.style.display = "none";
     });
 
-    // When the user clicks anywhere outside of the modal, close it
     window.addEventListener('click', (event) => {
         if (event.target == modal) {
             modal.style.display = "none";
         }
+    });
+    
+    // Form submission event
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const selectedCategoriesNames = Array.from(document.querySelectorAll('input[name="category"]:checked')).map(checkbox => checkbox.value);
+        const selectedCategoriesIds = selectedCategoriesNames.map(name => categoryIdMapping[name]);
+        console.log(selectedCategoriesIds);
+
+        // Send the category IDs to the server and update the rankings
+        fetch('http://localhost:4000/video/top_trending', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ categoryIDs: selectedCategoriesIds })
+        })
+        .then(response => response.json())
+        .then(data => {
+            updateVideoRankings(data.data);
+        })
+        .catch(error => {
+            console.error('Error posting category IDs:', error);
+        });
     });
 });
